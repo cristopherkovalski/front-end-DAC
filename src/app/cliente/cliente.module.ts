342c673db7cbb51f33ee5582f3f434752e1770c6
Vinicius Antunes <vinie.antuness@gmail.com>
2023-08-24 11:01:15 -0300

    arrumando mascara input deposito

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { CurrencyMaskDirective } from '../shared/directives/currency-mask.directive';
+// import { CurrencyMaskDirective } from '../shared/directives/currency-mask.directive';
 import { NumericoDirective } from '../shared/directives/numerico.directive';
 
 import { CurrencyMaskModule } from "ng2-currency-mask";
@@ -16,7 +16,6 @@ import { SaqueComponent } from './saque/saque.component';
   declarations: [
     DepositaComponent,
     NumericoDirective,
-    CurrencyMaskDirective,
     SaqueComponent
   ],
   imports: [
--- src/app/cliente/deposita/deposita.component.ts
+++ src/app/cliente/deposita/deposita.component.ts
@@ -13,7 +13,7 @@ export class DepositaComponent implements OnInit {
 
   cliente!: any;
   conta!: any;
-  public valorDeposito!: string;
+  public valorDeposito!: number;
 
   constructor(
     private clienteService: ClienteService
@@ -25,12 +25,9 @@ export class DepositaComponent implements OnInit {
   }
 
   depositar(): void {
-
-    let value = this.convertValue(this.valorDeposito);
-    console.log(value)
     
-    /*if ((this.formDepositar.form.valid) && ( value > 0)) {
-      if(this.clienteService.depositar(value)){
+    if ((this.formDepositar.form.valid) && (this.valorDeposito > 0)) {
+      if(this.clienteService.depositar(this.valorDeposito)){
         alert('Desposito realizado com succeso!');
 
         //pegando  a conta att só para visualizar
@@ -43,23 +40,24 @@ export class DepositaComponent implements OnInit {
       }
 
       
-    } */
+    }
   };
 
-  convertValue(value:string){
-    console.log(value)
-    // Remove o símbolo de moeda e espaços em branco
-    let numericValueString = value.replace(/[^\d,.-]/g, '');
+  // convertValue(value:string){
+  //   console.log(value)
+  //   console.log(typeof value);
+  //   // Remove o símbolo de moeda e espaços em branco
+  //   let numericValueString = value.replace(/[^\d,.-]/g, '');
 
-    // Substitui a vírgula por ponto para formatar como float
-    let numericValueFloat = parseFloat(numericValueString.replace(',', '.'));
+  //   // Substitui a vírgula por ponto para formatar como float
+  //   let numericValueFloat = parseFloat(numericValueString.replace(',', '.'));
 
-    console.log(numericValueFloat); // Saída: 1.23
-    return numericValueFloat
+  //   console.log(numericValueFloat); // Saída: 1.23
+  //   return numericValueFloat
 
-    //console.log(parseFloat(value.replace(/[^\d,.-]/g, '').replace(',', '.')))
-   // return parseFloat(value.replace(/[^\d,.-]/g, '').replace(',', '.'));
-  }
+  //   //console.log(parseFloat(value.replace(/[^\d,.-]/g, '').replace(',', '.')))
+  //  // return parseFloat(value.replace(/[^\d,.-]/g, '').replace(',', '.'));
+  // }
 
 
 }
