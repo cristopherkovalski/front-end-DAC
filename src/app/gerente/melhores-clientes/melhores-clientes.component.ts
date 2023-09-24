e52fe7a509b548230659c540eed4df14ac742e58
Gabriel Yan Russo <Gabriel.Yan.Russo@gmail.com>
2023-09-24 04:42:32 -0300

    R14: Consultar 3 melhores clientes - INCOMPLETA
    NÃO LIMITA A 3

--- src/app/gerente/melhores-clientes/melhores-clientes.component.ts
+++ src/app/gerente/melhores-clientes/melhores-clientes.component.ts
@@ -2,82 +2,100 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/shared/models/cliente.model';
 import { Usuario } from 'src/app/shared/models/usuario.model';
 import { GerenteService } from '../services/gerente.service';
+import { ClienteService } from 'src/app/cliente/services/cliente.service';
+import { Conta } from 'src/app/shared/models/conta.model';
 
 @Component({
   selector: 'app-melhores-clientes',
   templateUrl: './melhores-clientes.component.html',
   styleUrls: ['./melhores-clientes.component.css']
 })
-export class MelhoresClientesComponent implements OnInit{
+export class MelhoresClientesComponent implements OnInit {
 
-  constructor(private gerenteService:GerenteService){}
+
+  constructor(private gerenteService:GerenteService, private clienteService:ClienteService){}
 
   clientes: Cliente[] = [];
+  contas: Conta[] = [];
   gerente !: Usuario;
-
-
+  clientesFiltrados: Cliente[] = []; // Array para armazenar os clientes filtrados
+  consulta: string = ''; // A consulta de pesquisa inserida pelo usuário
+  
   ngOnInit(): void {
-      this.gerente = this.gerenteService.gerenteLogado();
-
-      //não gosto disso mas é o q tá tendo
-      this.buscaClientesAnalise();
-  }
-
-
-  buscaClientesAnalise(){
+    this.gerente = this.gerenteService.gerenteLogado();
     this.clientes = [];
-    this.gerenteService.getContasAprovacao(this.gerente.id!).subscribe(contas =>{
-      if (contas){
-        contas.forEach(conta => {
-          this.gerenteService.getClientesById(conta.id_cliente!).subscribe(cliente =>{
-            // console.log(cliente)
-            this.clientes.push(cliente)
-          })
-        });
-      }else
-        this.clientes = []
-    })
+    this.contas = [];
+    this.listarTodos();
+    this.listarTodosC();
+    this.filtrarClientesPorSaldo();
+   
   }
-  
 
-  aprovarCliente(cliente:Cliente){
-    if(confirm("Deseja Aprovar esse cliente?")){
-      
-      this.gerenteService.aprovarCliente(cliente).subscribe(response =>{
-        if(response){
-          alert("Senha do cliente:" + response.senha);
 
-          this.buscaClientesAnalise();
-        }else{
-          alert("Algo deu errado tente novamente mais tarde");
+  listarTodos(): Cliente[] {
+    this.gerenteService.listarTodosClientes().subscribe({
+      next: (data: Cliente[]) => {
+        if (data == null) {
+          this.clientes = [];
+        } else {
+          data.forEach((cliente) => {
+            this.clienteService.getAccontByClientId(cliente.id).subscribe((conta) => {
+              this.contas.push(conta);
+              if (conta.gerenteId == this.gerente.id) {
+                this.clientes.push(cliente);
+              }
+            });
+          });
         }
-        
-      })
-    }
-  };
-
-  
-
-  recusarCliente(cliente:Cliente){
-    console.log("Cliente reporvado");
-    if(confirm("Deseja realmente Recusar esse cliente?")){
-      let motivo = prompt("Por qual motivo está recusando esse cliente?");
-      if (motivo){
-        this.gerenteService.reprovarCliente(cliente, motivo).subscribe(response =>{
-          if(response){
-            alert("Conta recusada!");
-            this.buscaClientesAnalise();
-          } else
-            alert("Algo deu errado tente novamente mais tarde");
-        })
-      }else{
-        alert("Sem motivo sem recusa");
       }
-      
-    }
+    });
 
-  };
+    return this.clientes.sort((a, b) => a.nome.localeCompare(b.nome));
+  }
 
- 
+  listarTodosC(): Conta[] {
+    this.gerenteService.listarTodosContas().subscribe({
+      next: (data: Conta[]) => {
+        if (data == null) {
+          this.clientes = [];
+        }
+        else {
+          this.contas = data;
+          this.filtrarClientesPorSaldo();
+        }
+      }
+    });
+    return this.contas;
+  }
+
+  buscaSaldoConta(cliente: Cliente): any{
+    const clienteT = this.contas.filter(conta => conta.id_cliente == cliente.id).at(0)
+    return clienteT
+    
+  }
+
+
+  filtrarClientesPorSaldo() {
+    this.clientesFiltrados = this.clientes
+      .filter((cliente) => this.contas.some((conta) => conta.id_cliente === cliente.id))
+      .sort((a, b) => {
+        const saldoA = this.buscaSaldoConta(a)?.saldo || 0;
+        const saldoB = this.buscaSaldoConta(b)?.saldo || 0;
+  
+        if (saldoA >= 0 && saldoB >= 0) {
+          return saldoB - saldoA; 
+        } else if (saldoA < 0 && saldoB < 0) {
+          return saldoA - saldoB; 
+        } else {
+          return saldoA < saldoB ? 1 : -1; 
+        }
+      })
+      .slice(0, 3); 
+  }
+
+
+
+
+
 
 }
